be3bca3d628006de1859b6e70322dbfdc5dbf73a
Gitanshu <thegstudioz@gmail.com>
2026-07-04 23:06:30 +0530

    Sorting algorithms and

---
+++ Sorting/Bubble_sort/bubble_sort.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void bubble_sort(vector<int> &arr)
+{
+    int n = arr.size();
+    int temp = 0;
+    for (int i = 0; i < n-1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if(arr[j] > arr[j+1]){
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = arr[j];
+            }
+        }
+    }
+}
+int main()
+{
+    vector<int> arr = {1, 4, 2, 3, 5, 6};
+    bubble_sort(arr);
+
+    for (int i : arr)
+    {
+        cout << i << " ";
+    }
+
+    return 0;
+}
