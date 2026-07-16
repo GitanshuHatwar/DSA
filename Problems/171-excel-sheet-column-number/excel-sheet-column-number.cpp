d30e45ade64b328312ed38f9364d4f6e44d0935b
Gitanshu <143069682+GitanshuHatwar@users.noreply.github.com>
2026-07-16 16:15:56 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (15.48%) - LeetSync

--- Problems/171-excel-sheet-column-number/excel-sheet-column-number.cpp
+++ Problems/171-excel-sheet-column-number/excel-sheet-column-number.cpp
@@ -3,9 +3,7 @@ public:
     int titleToNumber(string columnTitle) {
         int sum = 0;
         for (char c : columnTitle) {
-            int num = c - 'A' + 1;
-            
-            sum = sum * 26 + num ;
+            sum = sum *  26 + (c - 'A' + 1);
         }
         return sum;
     }
