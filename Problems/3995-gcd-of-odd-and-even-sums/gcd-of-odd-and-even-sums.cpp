0eaecf069311c5d73eaba370cc52f06e8a55bf29
Gitanshu <143069682+GitanshuHatwar@users.noreply.github.com>
2026-07-18 12:11:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (83.67%) - LeetSync

---
+++ Problems/3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        int sumOdd = 0, sumEven = 0;
+        int even = 2, odd = 1;
+
+        for (int i = 0; i < n; i++) {
+            sumEven = sumEven + even;
+            even += 2;
+            sumOdd = sumOdd + odd;
+            odd += 2;
+        }
+
+        int a = max(sumEven, sumOdd);
+        int b = min(sumEven, sumOdd);
+        int rem = 0;
+
+        while (b != 0) {
+            rem = a % b;
+            a = b;
+            b = rem;
+        }
+        return a;
+    }
+};
