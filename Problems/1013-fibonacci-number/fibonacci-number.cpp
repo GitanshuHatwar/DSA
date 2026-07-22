6b6af27ddd2b22ce7e49305a3e70c7b135937635
Gitanshu <143069682+GitanshuHatwar@users.noreply.github.com>
2026-07-22 12:45:33 +0530

    Time: 2 ms (78.24%) | Memory: 7.8 MB (65.35%) - LeetSync

--- Problems/1013-fibonacci-number/fibonacci-number.cpp
+++ Problems/1013-fibonacci-number/fibonacci-number.cpp
@@ -1,9 +1,17 @@
 class Solution {
 public:
     int fib(int n) {
+        int fibo = 0;
         if(n == 0 || n == 1){
             return n;
         }
-        return fib(n-1) + fib(n-2);
+        int cur = 1;
+        int prev = 0;
+        for(int i = 1 ; i < n;i++){
+            fibo = cur + prev;
+            prev = cur ;
+            cur = fibo;
+        }
+        return fibo;
     }
 };
