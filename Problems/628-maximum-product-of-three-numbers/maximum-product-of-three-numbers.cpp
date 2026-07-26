b9f6129eb8fce90c497f9aa81ada9f4183e6a17d
Gitanshu <143069682+GitanshuHatwar@users.noreply.github.com>
2026-07-26 11:26:47 +0530

    Time: 14 ms (25.59%) | Memory: 31.5 MB (76.45%) - LeetSync

--- Problems/628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
+++ Problems/628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
@@ -4,9 +4,6 @@ public:
         int n = nums.size() - 1;
         sort(nums.begin(), nums.end());
 
-        int sum =  nums[n] * nums[n - 1] * nums[n - 2];
-        int base = nums[0] * nums[1] * nums[n];
-
-        return max(sum, base);
+        return max(nums[n] * nums[n - 1] * nums[n - 2], nums[0] * nums[1] * nums[n]);
     }
 };
