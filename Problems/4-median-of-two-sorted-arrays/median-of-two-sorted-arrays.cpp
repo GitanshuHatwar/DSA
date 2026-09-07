da8e61aaa08631b82e82168c06e5ab389385d40a
Gitanshu <143069682+GitanshuHatwar@users.noreply.github.com>
2026-09-07 14:44:24 +0530

    Time: 0 ms (100.00%) | Memory: 96.1 MB (9.39%) - LeetSync

--- Problems/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ Problems/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size();
-        int m = nums2.size();
 
-        vector<int> neww(m + n);
-         neww = nums1;
+        vector<int> neww;
+        neww = nums1;
         neww.insert(neww.end(), nums2.begin(), nums2.end());
 
         sort(neww.begin(), neww.end());
-
-        if ((n + m) % 2 != 0) {
-            int mid = (n + m) / 2;
+        int size = neww.size();
+        if (size % 2 != 0) {
+            int mid = size / 2;
             return neww[mid];
         } else {
-            int mid = (n + m) / 2;
+            int mid = size / 2;
 
             double mid1 = (neww[mid] + neww[mid - 1]) / 2.0;
             return mid1;
