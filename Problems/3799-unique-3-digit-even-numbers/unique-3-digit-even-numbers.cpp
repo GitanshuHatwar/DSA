d038ca244dfd80f03423678dbb85d9856af40153
Gitanshu <143069682+GitanshuHatwar@users.noreply.github.com>
2026-09-14 00:02:02 +0530

    Time: 0 ms (100.00%) | Memory: 28.6 MB (98.02%) - LeetSync

--- Problems/3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
+++ Problems/3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
-    int totalNumbers(auto& digits) {
-        int f[10] = {0};
-        int res = 0;
+    int totalNumbers(vector<int>& digits) {
+        vector<int> fq (10,0);
+        int res = 0 ;
 
-        for (auto& d : digits)
-            f[d]++;
+        for(auto nums : digits){
+            fq[nums]++;
+        }
 
-        for (int i = 1; i < 10; i++) 
-            for (int j = 0; j < 10; j++) 
-                for (int k = 0; k < 9; k += 2) 
-                    res += f[i] > 0 &&
-                           f[j] > (i == j) &&
-                           f[k] > (i == k) + (j == k);
-        
-
+        for(int i = 1 ; i < 10 ; i++){
+            for(int j = 0 ; j < 10 ;j++){
+                for(int k = 0 ; k < 9 ;k+=2){
+                    if(fq[i] == 0){
+                        continue;
+                    }
+                    if(fq[j] < 1 + (i == j)){
+                        continue;
+                    }
+                    if(fq[k] <  1 + (i==k) + (k==j)){
+                        continue;
+                    }
+                    res++;
+                }
+            }
+        }
         return res;
     }
 };
